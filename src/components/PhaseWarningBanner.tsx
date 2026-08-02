0ab900d21c96bb603aaa11fc9f5d0f11549370af
ChristoSweden <138971595+ChristoSweden@users.noreply.github.com>
2026-08-02 15:51:12 +0200

    Upgrade React Router and remediate advisories (src/components/PhaseWarningBanner.tsx)

--- src/components/PhaseWarningBanner.tsx
+++ src/components/PhaseWarningBanner.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 import { AlertTriangle, X } from 'lucide-react'
 import { loadProductData } from '@/lib/product-loader'
 
