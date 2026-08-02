0d465ac92125e13364220b9caaa0cb674ea034a8
ChristoSweden <138971595+ChristoSweden@users.noreply.github.com>
2026-08-02 15:51:15 +0200

    Upgrade React Router and remediate advisories (src/components/ProductPage.tsx)

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router'
 import { loadProductData } from '@/lib/product-loader'
 import { AppLayout } from '@/components/AppLayout'
 import { EmptyState } from '@/components/EmptyState'
